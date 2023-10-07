92f6566a087987373d81640024411c4795aba358
Sahil-Shadwal <sahilshadwal@gmail.com>
2023-10-07 18:21:23 +0530

    static routing using nextjs13

---
+++ app/about/page.tsx
@@ -0,0 +1,17 @@
+// export default function page() {
+//   return (
+//     <main>
+//       <h1>About</h1>
+//     </main>
+//   );
+// }
+
+const page = () => {
+  return (
+    <main>
+      <h1>About</h1>
+    </main>
+  );
+};
+
+export default page;
---
+++ app/projects/list/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <main>
+      <h1>ProjectList</h1>
+    </main>
+  );
+};
+
+export default page;
